129ea9e596e27ac51d137e6295201b2a458d16b1
XintongTheCoder <xintongmi@gmail.com>
2023-04-17 21:07:45 -0700

    complete plunkFirstLineFromFile

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -4,10 +4,46 @@
 
 var fs = require('fs');
 var request = require('needle');
+const readline = require('readline');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  // fs.exists(filePath, (exists) => {
+  //   let err = new Error('File does not exist');
+  //   err.code = 'ENOENT';
+  //   if (!exists) {
+  //     callback(err);
+  //   } else {
+  //     let lineReader = readline.createInterface({
+  //       input: fs.createReadStream(filePath),
+  //     });
+  //     let lineNo = 0;
+  //     lineReader.on('line', (line) => {
+  //       lineNo++;
+  //       callback(null, line);
+  //       if (lineNo === 1) {
+  //         lineReader.close();
+  //         lineReader.removeAllListeners();
+  //       }
+  //     });
+  //   }
+  // });
+  const readStream = fs.createReadStream(filePath);
+
+  readStream
+    .on('error', (err) => {
+      callback(err);
+    })
+    .on('ready', () => {
+      let lineReader = readline.createInterface({
+        input: readStream,
+      });
+      lineReader.on('line', (line) => {
+        lineReader.close();
+        lineReader.removeAllListeners();
+        callback(null, line);
+      });
+    });
 };
 
 // This function should retrieve the status code of a GET request to `url`
@@ -18,5 +54,5 @@ var getStatusCode = function (url) {
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
   getStatusCode: getStatusCode,
-  pluckFirstLineFromFile: pluckFirstLineFromFile
+  pluckFirstLineFromFile: pluckFirstLineFromFile,
 };
